7c22ce45763af6588f3a17b6db5fe10f835ed125
Ravi Kiran <ravikiran.anumula@mediakind.com>
2019-07-29 17:53:37 +0530

    Added Router

--- src/app.js
+++ src/app.js
@@ -1,33 +1,12 @@
 import express from "express";
 import QueryParser from "./middlewares/queryParser";
 import CookieParser from "./middlewares/cookieParser";
+import { router } from './routes';
 
 const app = express();
+
 app.use(QueryParser);
 app.use(CookieParser);
-
-app.get('/', function (req, res) {
-    res.send('Invalid Route');
-});
-
-app.get('/api/products', function (req, res) {
-    res.json([{ id: 1, name: "P1" }, { id: 2, name: "P2" }]);
-});
-
-app.get('/api/products/:id', function (req, res) {
-    res.json({ id: req.params.id, name: `P${req.params.id}` });
-});
-
-app.get('/api/products/:id/reviews', function (req, res) {
-    res.json({ id: req.params.id, reviews: "Excellent Product" });
-});
-
-app.post('/api/products', function (req, res) {
-    res.send([{ id: 1, name: "P1" }, { id: 2, name: "P2" }]);
-});
-
-app.get('/api/users', function (req, res) {
-    res.send([{ id: 1, name: "U1" }, { id: 2, name: "U2" }]);
-});
+app.use(router);
 
 export default app;
